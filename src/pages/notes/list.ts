0ea3c445908d21823f26cbb8e5d91472adf56208
Ginpei <ginpei@ginpei.dev>
2024-10-06 22:20:56 -0700

    list page

--- src/pages/notes/list.ts
+++ src/pages/notes/list.ts
@@ -1,8 +1,16 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 
-export const noteListRoute = new Hono();
+type Env = {
+	Bindings: {
+		NOTES: KVNamespace;
+	};
+};
 
-noteListRoute.get("/notes", (c) => {
+export const noteListRoute = new Hono<Env>();
+
+noteListRoute.get("/notes", async (c) => {
+	const notes = await getNotes(c);
+
 	return c.html(
 		/* html */`
 <!DOCTYPE html>
@@ -18,12 +26,31 @@ noteListRoute.get("/notes", (c) => {
 		<a href="/notes/new">New note</a>
 	</p>
   <ul>
-    <li>
-      <small>(No notes)</small>
-    </li>
+		${notes.map((note) => /* html */`
+			<li>
+				<a href="/notes/${note.id}">${note.title}</a>
+			</li>
+		`.trim()).join('\n')}
   </ul>
 </body>
 </html>
 		`.trim()
 	);
 });
+
+// TODO extract
+async function getNotes(c: Context<Env>): Promise<{ id: string; title: string; }[]> {
+	const keys = await c.env.NOTES.list();
+	const notes: { id: string; title: string }[] = [];
+
+	// 各キーに対してノートのデータを取得
+	for (const key of keys.keys) {
+		const note = await c.env.NOTES.get(key.name);
+		if (note) {
+			const noteData = JSON.parse(note);
+			notes.push({ id: key.name, title: noteData.title });
+		}
+	}
+
+	return notes;
+}
